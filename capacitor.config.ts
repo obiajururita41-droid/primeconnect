15b4ec2b98bc59ae8d0b6b6d3c5a981a64902f62
Rita Obiajuru <ritaobiajuru8@gmail.com>
2026-06-26 09:00:47 +0100

    Add allowMixedContent for Android WebView

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.primeconnect.app',
   appName: 'PrimeConnect',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
